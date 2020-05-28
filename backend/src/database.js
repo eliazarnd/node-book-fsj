6631d98abd6103c4eb877037564591b799d5d1fa
Eliazar Lopez Robles <eliazar.lopez@corvuscode.com>
2020-05-28 08:54:08 -0700

    I added a new configuration for env var

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/booksApp", {
+  .connect(process.env.MONGO_DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
