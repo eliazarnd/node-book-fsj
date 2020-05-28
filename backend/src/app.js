66aea9eb6e1de6d9bde5a72258d45becdbe35e55
Eliazar Lopez Robles <eliazar.lopez@corvuscode.com>
2020-05-28 08:26:45 -0700

    I added all middlewares for my server, morgan, multer, router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,41 @@
 const express = require("express");
 const morgan = require("morgan");
-const app = express();
 const path = require("path");
-const indexRouter = require("./routes/index");
+const multer = require("multer");
+
+//Initializations
+const app = express();
+//Connection mongo database
+require("./database");
+
+//Getting routes
+const booksRouter = require("./routes/books");
 
 //Settings
 app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
+//Upload with multer, setting for multer
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "public/uploads"),
+  filename(req, file, cb) {
+    cb(null, new Date.getTime() + path.extname(file.originalname));
+  },
+});
 
-app.use("/", indexRouter);
+//Middlewares
+app.use(morgan("dev"));
+app.use(multer({ storage }).single("image"));
+//This middleware is to interpreted information like a json
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.use("/api/v1/books", booksRouter);
 
 app.listen(app.get("port"), () => {
   console.log("Server on port", app.get("port"));
